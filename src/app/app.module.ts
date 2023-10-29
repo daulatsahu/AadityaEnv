1da60cf2acd4a0bda7b264a7b346aeaad9635208
Daulat <daulatsahu5252@gmail.com>
2023-10-29 23:17:01 +0530

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StartpageComponent } from './components/startpage/startpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartpageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
